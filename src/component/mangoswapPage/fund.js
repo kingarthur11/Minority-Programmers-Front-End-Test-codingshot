4b757742033141d2c6f88bf9536dc1c56c95a855
arthur orduh <arthurorduh@gmail.com>
2021-05-30 22:55:54 +0100

    mangoswap page

--- src/component/mangoswapPage/fund.js
+++ src/component/mangoswapPage/fund.js
@@ -11,14 +11,14 @@ const Mangoswap = () => {
                         <div className="mangoswap-logo">
                             <i class="fas fa-arrow-left left-arrow-mangoswap"></i>
                             <img src={mangoswapImage} alt="name" />
-                            <h3 className="mangoswap-header-h3">Mangoswap</h3>
+                            <h3 className="mangoswap-logo-h3">Mangoswap</h3>
                         </div>
-                        <div >
-                            <div className="mangoswap-fund-raised">
+                        <div>
+                            <div className="mangoswap-logo-fund">
                                 <h4>Funds raised</h4>
                                 <p>50% complete</p>
                             </div>
-                            <h3>$200,000/$400,000</h3>
+                            <h3 className="mangoswap-logo-h3">$200,000/$400,000</h3>
                         </div>
                         <div>
                             <hr className="mangoswap-horizontal-colo"/>
@@ -28,17 +28,68 @@ const Mangoswap = () => {
                     </div>
                 </div>
                 <div className="mangoswap-body">
-                    <div className="mangoswap-sidebar">helo</div>
+                    <div className="mangoswap-sidebar">
+                        <div className="mangoswap-sidebar-header">
+                            <p>Founded 17th May, 2015</p>
+                        </div>
+                        <hr />
+                        <div className="mangoswap-sidebar-contact">
+                            <small>Website</small>
+                            <p>
+                                mangoswap.com
+                                <span>
+                                    <i class="fab fa-twitter mangoswap-fontawsome"></i>
+                                    <i class="fab fa-instagram mangoswap-fontawsome"></i>
+                                    <i class="fab fa-facebook-square mangoswap-fontawsome"></i>
+                                </span>
+                            </p>
+                        </div>
+                    </div>
                     <div className="mangoswap-about">
                         <h3>About startup</h3>
                         <p className="mangoswap-p">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint</p>
                         <hr />
-                        <h3 className="mangoswap-h3">Vision</h3>
+                        <h3>Vision</h3>
                         <p className="mangoswap-p">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint</p>
                     </div>
                 </div>
             </div>
         </div>
+        // <div className="mangoswap-content">
+        //     <div>
+        //         <div className="mangoswap-header-content">
+        //             <div className="mangoswap-header">
+        //                 <div className="mangoswap-logo">
+        //                     <i class="fas fa-arrow-left left-arrow-mangoswap"></i>
+        //                     <img src={mangoswapImage} alt="name" />
+        //                     <h3 className="mangoswap-header-h3">Mangoswap</h3>
+        //                 </div>
+        //                 <div >
+        //                     <div className="mangoswap-fund-raised">
+        //                         <h4>Funds raised</h4>
+        //                         <p>50% complete</p>
+        //                     </div>
+        //                     <h3>$200,000/$400,000</h3>
+        //                 </div>
+        //                 <div>
+        //                     <hr className="mangoswap-horizontal-colo"/>
+        //                     <hr className="mangoswap-horizontal-gray"/>
+        //                 </div>
+        //                 <button className="fund-mangoswap-button">Fund Startup</button>
+        //             </div>
+        //         </div>
+        //         <div className="mangoswap-body">
+        //             <div className="mangoswap-sidebar">helo</div>
+        //             <div className="mangoswap-about">
+        //                 <h3>About startup</h3>
+        //                 <p className="mangoswap-p">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint</p>
+        //                 <hr />
+        //                 <h3 className="mangoswap-h3">Vision</h3>
+        //                 <p className="mangoswap-p">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint</p>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
     )
 }
 
